baface458c02973431bcc775c6fe269aa657c095
evilmuscle <346721300@qq.com>
2025-01-17 10:53:29 +0800

    Initial commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const Page: FC = () => {
+  return <p>Customers Page</p>;
+};
+
+export default Page;
---
+++ app/ui/font.ts
@@ -0,0 +1,4 @@
+import { Borel,Lusitana } from 'next/font/google';
+ 
+export const inter = Borel({ subsets: ['latin'] ,weight: "400", style: "normal" });
+export const lus = Lusitana({ subsets: ['latin'] ,weight: "400", style: "normal" });
